695321071c5e913469bfd6c4be0aff09fb7302dc
Baha <88669412+JegBaha@users.noreply.github.com>
2025-05-08 11:55:27 +0300

    Behold First Offeing.

--- YazilimDeneme/Form1.cs
+++ YazilimDeneme/Form1.cs
@@ -56,7 +56,13 @@ namespace YazilimDeneme
             this.Controls.Add(homeLabel);
         }
 
-        
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            // Giriş Yap butonuna tıklandığında
+            welcomeLabel.Visible = false;
+            enterButton.Visible = false;
+            homeLabel.Visible = true;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
